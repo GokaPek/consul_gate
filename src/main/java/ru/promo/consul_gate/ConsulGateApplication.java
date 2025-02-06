05069fc89bc6a77806da08fabe65319af094cfc2
GokaPek <Hgogogo@yandex.ru>
2025-02-06 23:20:16 +0400

    init

---
+++ src/main/java/ru/promo/consul_gate/ConsulGateApplication.java
@@ -0,0 +1,13 @@
+package ru.promo.consul_gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulGateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/promo/consul_gate/ConsulGateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.promo.consul_gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
