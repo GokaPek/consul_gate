486ad857e69006c7a9f507a74fcb99878279a634
GokaPek <Hgogogo@yandex.ru>
2025-02-07 11:09:56 +0400

    Использование реактивного подхода

--- src/main/java/ru/promo/consul_gate/ConsulGateApplication.java
+++ src/main/java/ru/promo/consul_gate/ConsulGateApplication.java
@@ -2,8 +2,10 @@ package ru.promo.consul_gate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.yml")
 public class ConsulGateApplication {
 
 	public static void main(String[] args) {
